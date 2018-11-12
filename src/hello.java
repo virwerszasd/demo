6df198316253d8c6917f425a247e89132ff374f4
Tong <1091272482@qq.com>
2018-11-12 16:28:44 +0800

    create repository

---
+++ src/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+        String[] arage = new String[10];
+        String str = arage[10];
+        System.out.println(str);
+    }
+}
